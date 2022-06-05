ac349a129b9aaec67c1a74f0ea23d944cb880898
Immanuel-Victor <immanuel.victor@academico.ifpb.edu.br>
2022-06-04 21:46:35 -0300

    atvd: adiciona questao 5 do Baruque e Baruque

---
+++ CBBExercicio5.java
@@ -0,0 +1,16 @@
+
+public class CBBExercicio5 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Os pares de 0 a 100 serão mostrados:  ");
+		
+		for(int i = 0; i <= 100; i++) {
+			if(i%2 == 0) {
+				System.out.printf("%d \n", i);
+			}
+		}
+
+	}
+
+}
