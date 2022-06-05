fd5caf3891f6a8551a5bd5c9f2a3ea3b53fc1a0e
Immanuel-Victor <immanuel.victor@academico.ifpb.edu.br>
2022-06-04 21:42:32 -0300

    atvd: adiciona questao 4 do livro Baruque e Baruque

---
+++ CBBExercicio4.java
@@ -0,0 +1,14 @@
+
+public class CBBExercicio4 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Os números serão contados de 0 a 100: ");
+		
+		for(int i = 0; i <= 100; i++) {
+			System.out.printf("%d \n", i);
+		}
+
+	}
+
+}
